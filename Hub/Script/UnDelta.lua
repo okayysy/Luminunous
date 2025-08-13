c87b593346ac08710eadea4c21f942f6177445d4
okayysy <yuannprince@gmail.com>
2025-08-13 18:11:12 +0800

    Update and rename UnpDelta.lua to UnDelta.lua

--- Hub/Script/UnDelta.lua
+++ Hub/Script/UnDelta.lua
@@ -1,3 +1,2 @@
 loadstring(game:HttpGet("https://pastefy.app/rNQ4m8Oa/raw"))()
-(1-2)=Connected
 loadstring(game:HttpGet("https://raw.githubusercontent.com/okayysy/Middle-Pet/refs/heads/main/Raw/Script/EzraHub.lua"))()
